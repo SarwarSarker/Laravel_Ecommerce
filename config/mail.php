714ffb964a348191c8cca65a6f2168f13922cfb5
sarwar <sarwarsarker00@gmail.com>
2020-09-27 12:41:20 +0600

    bug fix

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'sarwarsarker00@gmail.com'),
-        'name' => env('MAIL_FROM_NAME', 'Laravel E_commerce'),
+        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
+        'name' => env('MAIL_FROM_NAME', 'Example'),
     ],
 
     /*
