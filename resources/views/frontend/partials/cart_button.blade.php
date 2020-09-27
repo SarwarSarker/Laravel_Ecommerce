445dd05a5cf7e4f0dd6122b84f9496583daa8f7a
sarwar <sarwarsarker00@gmail.com>
2020-09-27 19:44:02 +0600

    bug fix

--- resources/views/frontend/partials/cart_button.blade.php
+++ resources/views/frontend/partials/cart_button.blade.php
@@ -1,3 +1,8 @@
+<form action="{{route('carts.store')}}" method="post">
+    @csrf
 <input type="hidden" name="product_id" value="{{ $row->id }}">
-<button type="button" class="flex-c-m size1 bg4 bo-rad-23 hov1 s-text1 trans-0-4"
-    onclick="addToCart({{ $row->id }})">Add to Cart</button>
+<button type="submit" class="flex-c-m size1 bg4 bo-rad-23 hov1 s-text1 trans-0-4"
+    >Add to Cart</button>
+</form>
+
+{{-- onclick="addToCart({{ $row->id }})" --}}
